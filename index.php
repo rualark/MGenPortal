330dae2adbdcec4efd47d37ca691bb72202160de
Rualark <rualark@gmail.com>
2017-12-10 01:35:34 +0300

    Load_user

--- index.php
+++ index.php
@@ -4,21 +4,10 @@ require_once "lib/config.php";
 require_once "lib/auth.php";
 
 if (login()) {
-  echo "Your session is logged in<br>";
+  echo "Your session is logged in, $ua[u_name]<br>";
   echo "<a href='login.php?action=out'>Sign out</a>";
 }
 else {
-  if(isset($_POST['log_in'])) {
-    if (enter()) {
-      echo "Error logging in";
-    }
-    else {
-      echo "You logged in successfully<br>";
-      die ("<script language=javascript>location.replace('index.php');</script>");
-    }
-  }
-  else {
-    echo "Hello guest! Please <a href='login.php'>login</a><br>";
-  }
+  echo "Hello guest! Please <a href='login.php'>login</a><br>";
 }
 ?>
