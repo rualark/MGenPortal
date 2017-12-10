71199d0af622889f9e008b8a79d5fadd5e3fbfa9
Rualark <rualark@gmail.com>
2017-12-10 19:20:52 +0300

    Further development

--- template/menu.php
+++ template/menu.php
@@ -19,30 +19,49 @@
 <body>
 
 <nav class="navbar navbar-expand-md navbar-black fixed-top">
-  <a class="navbar-brand" href="index.php">Composer Tracker</a>
+  <a class="navbar-brand" href="index.php"><b>Composer Tracker</b></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-        <div class="dropdown-menu" aria-labelledby="dropdown01">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
+      <?php
+      if ($uid) {
+        ?>
+        <li class=nav-item><a class=nav-link href="upload.php"><b>Upload</b></a></li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Uploads</a>
+          <div class="dropdown-menu" aria-labelledby="dropdown01">
+            <a class="dropdown-item" href="uploads.php">Uploads</a>
+            <a class="dropdown-item" href="passages.php">Passages</a>
+            <a class="dropdown-item" href="status.php">Servers status</a>
+          </div>
+        </li>
+        <li class=nav-item><a class=nav-link href="explore.php">Explore</a></li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?=$ua['u_name'] ?></a>
+          <div class="dropdown-menu" aria-labelledby="dropdown01">
+            <a class="dropdown-item" href="profile.php">Profile</a>
+            <a class="dropdown-item" href="login.php?action=out">Sign out</a>
+          </div>
+        </li>
+        <?php
+      }
+      else {
+        ?>
+        <li class=nav-item><a class=nav-link href="login.php"><b>Login</b></a></li>
+        <li class=nav-item><a class=nav-link href="reg.php"><b>Sign up</b></a></li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Uploads</a>
+          <div class="dropdown-menu" aria-labelledby="dropdown01">
+            <a class="dropdown-item" href="uploads.php">Uploads</a>
+            <a class="dropdown-item" href="passages.php">Passages</a>
+          </div>
+        </li>
+        <?php
+      }
+      ?>
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
