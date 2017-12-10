1853ec88aebd9adb6dd2d8c5a2b172dfae29fd9f
Rualark <rualark@gmail.com>
2017-12-10 15:04:32 +0300

    Created main page bootstrap design

--- index.php
+++ index.php
@@ -3,12 +3,16 @@ require_once "lib/lib.php";
 require_once "lib/config.php";
 require_once "lib/auth.php";
 
+$title = "Composer Tracker";
+
 if (login()) {
   echo "Your session is logged in, $ua[u_name]<br>";
   echo "<a href='login.php?action=out'>Sign out</a>";
 }
 else {
-  echo "Hello guest! Please <a href='login.php'>login</a> or register:<br>";
-  show_reg_form();
+  //echo "Hello guest! Please <a href='login.php'>login</a> or register:<br>";
+  //show_reg_form();
 }
+
+include "template/menu.php";
 ?>
