260ff6fecc0ce1f7e0c0502952d332e56c92190d
Rualark <rualark@gmail.com>
2017-12-11 00:56:22 +0300

    Improve code quality

--- upload.php
+++ upload.php
@@ -4,7 +4,7 @@ require_once "lib/config.php";
 require_once "lib/auth.php";
 require_once "lib/reports.php";
 
-$title = "Composer Tracker: Upload";
+$title = "Composer Tracker: File";
 $f_id = secure_variable("f_id");
 
 login();
