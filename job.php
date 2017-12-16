3d0d0d0efa996673b037bbaf23f5a9def6f47374
Rualark <rualark@gmail.com>
2017-12-17 01:19:57 +0300

    Use ajax to periodically update job state in file.php and job.php

--- job.php
+++ job.php
@@ -3,6 +3,7 @@ require_once "lib/mlib.php";
 require_once "lib/config.php";
 require_once "lib/auth.php";
 require_once "lib/reports.php";
+require_once "lib/ajax.php";
 
 $title = "$site_name: Job";
 $j_id = secure_variable("j_id");
@@ -21,4 +22,5 @@ show_job();
 show_job_editor();
 
 include "template/footer.php";
+periodic_update("ajax/jstate.php?j_id=$j_id", 1000);
 ?>
