e7defab73d3aba323165e91ee27ee1a2f6539f82
Rualark <rualark@gmail.com>
2017-12-21 21:32:23 +0300

    More security

--- job.php
+++ job.php
@@ -11,12 +11,16 @@ $j_id = secure_variable("j_id");
 login();
 
 include "template/menu.php";
+load_job();
+load_job_config();
+
+if ($uid != $wj['u_id'] && $wj['f_private'] && !$ua['u_admin']) {
+  echo "<p class=text-danger><b>Sorry, you do not have access to this upload, because it was marked private by user.</b></p>";
+  exit;
+}
 
 echo "<br>";
 echo "<div class=container>";
-
-load_job();
-load_job_config();
 
 show_job();
 show_job_editor();
